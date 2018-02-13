dc250889c59b830f987cf384ed55cbd30e85ff90
bonnie519 <beigao519@gmail.com>
2018-02-12 21:02:53 -0800

    add heart

---
+++ Assets/Heart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		//increase score via some UIManager
+		//destroy this gameobject
+		Debug.Log("you got a heart");
+		PlayerPrefs.SetInt ("LIFE", PlayerPrefs.GetInt ("LIFE") + 1);
+		Destroy (this.gameObject);
+	}
+}
